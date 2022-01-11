d89fd4dc189a793319ee91d7796e48890b150ad6
Martin Michalko <mmichalk>
2022-01-11 21:53:16 +0000

    commit merge

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -3,19 +3,21 @@
 #make clean && make sincos   # Rebuild the sincos executable.
 #./sincos > /tmp/sin.dat     # Place the data points into a file.
 
-rm -f /tmp/length.dat
 
-for n in {1..100}; do
-	echo -n $n >> /tmp/length.dat
-	./collatz -n $n | wc -l >> /tmp/length.dat
+rm -f /tmp/max.dat
+
+for n in {1..1000}; do
+       ./collatz | sort -nr | head -n 1 >> /tmp/max.dat	
 done
 
 # This is the heredoc that is sent to gnuplot.
+
 gnuplot <<END
     set terminal pdf
-    set output "length.pdf"
-    set title "Sequence lengths"
-    set xlabel "x"
-    set ylabel "y"
-    plot "/tmp/length.dat" with points title ""
+    set output "max.pdf"
+    set title "Maximum Collatz Sequence Value"
+    set xlabel "n"
+    set ylabel "value"
+    plot "/tmp/max.dat" with points title ""
 END
+
