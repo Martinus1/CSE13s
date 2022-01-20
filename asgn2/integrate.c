81dfccaa31ccaa6f561403b1fd269edc6f56a2bc
Martin Michalko <mmichalk>
2022-01-20 03:54:01 +0000

    Finished Code Works

--- asgn2/integrate.c
+++ asgn2/integrate.c
@@ -30,6 +30,7 @@ int main(int argc, char **argv) {
 	double high = 0.0;
 	double low = 0.0;
 	double (*function)(double) = a;
+	
 
 	int opt = 0;
 	while((opt = getopt(argc, argv, "abcdefghij:n:p:q:H:")) != -1) {
@@ -74,7 +75,8 @@ int main(int argc, char **argv) {
 				high = strtod(optarg, NULL);
 				break;
 			case 'H':
-				usage(argv[0]); return EXIT_FAILURE;
+				usage(argv[0]); 
+				return EXIT_FAILURE;
 			default:
 				fprintf(stderr, "error/n");
 				return 1;
@@ -88,32 +90,55 @@ int main(int argc, char **argv) {
 		fprintf(stderr,"error/n");
 		return 1;
 	}
+	return 0;
 
 }
 
 
 double integrate(double (*f)(double x), double a, double b, uint32_t n) {
-	double step = (a-b) / n;
-	double res = 0;
+	double step = (b-a) / n;
+	double res = 0.0;
 
-	for (uint32_t i = 0; i <= n; i += 1) {
-		double x = a + (i * step);
-		double fx = (*f)(x);
-		if (i == 0 || i == n) {
-			res += fx;
-		} else if (i % 2 != 0) {
-			res += 4*fx;
-		} else {
-			res += 2*fx;
-		}
-		
+	printf("low = %lf\n",a);
+	printf("hight = %lf\n",b);
+	printf("partitions = %u\n",n);
+	
+	double fx[n];
+	double x[n];
+
+	for(uint32_t i = 0; i <= n; i += 1) {
+		x[i] = a + i * step;
+		fx[i] = f(x[i]);
+		printf("element %u,", i);
+		printf("x = %f,", x[i]);
+		printf("fx = %f \n", fx[i]);
 	}
 
-	res = (b/3) * res;
 
+	for (uint32_t i = 0; i <= n; i += 1) {
+		double x = a + i * step;
+		double fx = f(x);
+		printf("element %u, ", i);
+		printf("x = %f, ", x);
+		printf("fx = %f \n ", fx);
+	
+		if (i == 0 || i == n) {
+			printf("0/last i = %lf\n", fx);
+			res += fx;
+		} else if (i % 2 != 0) {
+			printf("4i = %lf\n", (4*fx));
+			res += 4*fx;
+		} else {
+			printf("2i = %lf\n", (2*fx));
+			res += 2*fx;
+		}		
+	}
+
+	res = res * (step/3);
+	printf("step = %lf", (step/3));
+	printf("result = %lf\n", res);
 	return res;
 }
-
 
 
 
--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include "mathlib.h"
 #include <stdlib.h>
-
-#define EPSILON 1e-14
+#include <math.h>
+#include "mathlib.h"
 
 double Exp(double x) {
-	double  trm = 1.0;
-	double  sum = trm;
+	double trm = 1.0;
+	double sum = trm;
 	double k = 1;
 	while(trm > EPSILON) {
-		trm *= abs(x) / k;
+		trm = trm * (fabs(x) / k);
 		sum += trm;	
 		k += 1;
 	}
 
-	if x > 0 {
+	if (x > 0) {
 		return sum;
 	} else {
 		return (1/sum);
@@ -25,9 +24,9 @@ double Sin(double x) {
 	double s = 1.0;
 	double v = x;
 	double t = x;
- 	double k = 3.0:
+ 	double k = 3.0;
 
-	while(abs(t) > EPSILON) {
+	while(fabs(t) > EPSILON) {
 		t = t*((x*x) / ((k-1)*k));
 		s = -s;
 		v += s*t;
@@ -40,14 +39,14 @@ double Sin(double x) {
 double Cos(double x) {
 	double s = 1.0;
 	double v = 1.0;
-	double t = x;
-	double k = 1.0;
+	double t = 1.0;
+	double k = 2.0;
 
-	while(abs(t) > EPSILON) {
-		t = t*((t*(x*x) )/((k-1)*k));
+	while(fabs(t) > EPSILON) {
+		t = t*((x*x) / ((k-1)*k));
 		s = -s;
 		v += s*t;
-		k += 2.0
+		k += 2.0;
 	}
 	
 	return v;
@@ -58,7 +57,7 @@ double Sqrt(double x) {
 	double z = 0.0;
 	double y = 1.0;
 
-	while(abs(y-z) > EPSILON) {
+	while(fabs(y-z) > EPSILON) {
 		z = y;
 		y = 0.5 * (z + (x / z));
 	}
@@ -69,8 +68,8 @@ double Sqrt(double x) {
 double Log(double x) {
 	double y = 1.0;
 	double p = Exp(y);
-	while(abs(p-x) > EPSILON) {
-		y = (y+x)/(p-1);
+	while(fabs(p-x) > EPSILON) {
+		y = y + x / p - 1;
 		p = Exp(y);
 	}
 
