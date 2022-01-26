b8bdacf2697baea29f53c7978440dfa6fa2e6b4d
Martin Michalko <mmichalk>
2022-01-26 07:21:57 +0000

    Testing Insert

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -1,17 +1,19 @@
 #include <math.h>
 #include <stdio.h>
 #include "insert.h"
+#include "stats.h"
 
 
 void insertion_sort(Stats *stats, uint32_t *A, uint32_t n){
-	for (i = 1; i < n;  ++i) {
+	for (uint32_t i = 1; i < n;  ++i) {
 		int j = i;
-		int temp = A[i];
-		
+		uint32_t temp = A[i];
 		while (j > 0 && temp < A[j-1]) {
 			A[j] = A[j - 1];
 			j -= 1;
+			stats -> compares += 1;			
 		}
 		A[j] = temp;
 	}
+
 }
---
+++ asgn3/sorting.c
@@ -0,0 +1,105 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <getopt.h>
+#include "quick.h"
+#include "heap.h"
+#include "insert.h"
+#include "stats.h"
+#include "batcher.h"
+#include "set.h"
+#include "stats.h"
+
+typedef enum {
+	INSERT = 0,
+	HEAP = 1,
+	QUICK = 2,
+	BATCH = 3
+} Sort;
+
+
+
+int main(int argc, char **argv) {
+	
+	uint32_t n = 100;
+	int seed = 13371453;
+	
+	Set s = empty_set();
+	
+	int opt = 0;
+	while((opt = getopt(argc, argv, "ahbiq:r:n:p:H")) != -1 ) {
+		switch (opt) { 
+			case 'a':
+				s = complement_set(empty_set());
+				break;
+			case 'h':
+				s = insert_set(HEAP, s);
+				break;
+			case 'b':
+				s = insert_set(BATCH, s);
+				break;
+			case 'i':
+				s = insert_set(INSERT, s);
+				break;
+			case 'q':
+				s = insert_set(QUICK, s);
+				break;
+			case 'r':
+				srand(time(NULL));
+				seed = rand();
+				break;
+			case 'n':
+				n = atoi(optarg);
+				break;
+			case 'p':
+				break;
+			case 'H':
+				break;
+			default:
+				fprintf(stderr, "error/n");
+				return 1;
+		}
+	}
+
+
+	uint32_t *A = (uint32_t *)malloc(n * sizeof(uint32_t));
+
+	for (uint32_t i = 0; i < n; i += 1) {
+		A[i] =  rand();
+	}
+
+	free(A);
+	
+	Stats record;
+	record.moves = 0;
+	record.compares = 0;
+
+	for (Sort i = INSERT; i <= BATCH; i += 1) {
+		if(member_set(i, s)) {
+			switch(i) {
+				case INSERT:
+					insertion_sort(&record, A, n);
+					printf("Insertion Sort, ");
+					printf("%u elements, ", n);
+					printf("%lu moves, ", record.moves);
+					printf("%lu compares, \n ", record.compares);
+					for(uint32_t v = 0; v < n; v ++) {
+						printf("%u     ", A[i]);
+					}
+					break;
+				case HEAP:
+					break;
+				case QUICK:
+					break;
+				case BATCH:
+					break;
+				default:
+					break;
+			}
+		}
+	
+	}
+
+}
+
