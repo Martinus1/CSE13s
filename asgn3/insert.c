215f66e53f681b182109e75eedf5a8b5749cac69
Martin Michalko <mmichalk>
2022-01-27 02:55:11 +0000

    ins stats

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -8,11 +8,18 @@ void insertion_sort(Stats *stats, uint32_t *A, uint32_t n){
 	for (uint32_t i = 1; i < n;  ++i) {
 		int j = i;
 		uint32_t temp = A[i];
-		while (j > 0 && temp < A[j-1]) {
+		move(stats, A[i]);
+		//cmp(stats, j, 0);
+		//cmp(stats, temp, A[j-1]);
+		while ( j > 0 && cmp(stats, A[j - 1],temp) > 0) {
+			//cmp(stats,temp, A[j - 1]);
+			//23cmp(stats, j, 0);
+			move(stats, A[j - 1]);
 			A[j] = A[j - 1];
 			j -= 1;
-			stats -> compares += 1;			
+						
 		}
+		move(stats,A[j]);
 		A[j] = temp;
 	}
 
