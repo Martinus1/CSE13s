c5121ef62924ee269fe24c059ff785d7cef350df
Martin Michalko <mmichalk>
2022-02-03 03:43:03 +0000

    Clang Format

--- asgn4/life.c
+++ asgn4/life.c
@@ -10,143 +10,139 @@
 
 #define DELAY 50000
 
-
 int main(int argc, char **argv) {
 
-       
-    	uint32_t n = 100;
-        bool isToroidal = false;
-        bool isNcurses = true;
-        int opt = 0;
-	char *ifile;
-	bool out_print = false;
-	char *ofile;
+    uint32_t n = 100;
+    bool isToroidal = false;
+    bool isNcurses = true;
+    int opt = 0;
+    char *ifile;
+    bool out_print = false;
+    char *ofile;
 
-        while((opt = getopt(argc, argv, "tsn:i:o:")) != -1 ) {
-                switch (opt) {
-                        case 't':
-                                //toggle toriodal on
-                                isToroidal = true;
-                                break;
-                        case 's':
-                                //Toggle Silence ncurses off
-                                isNcurses = false;
-                                break;
-                        case 'n':
-                                //specifies the number of generations
-                                n = atoi(optarg);
-                                break;
-                        case 'i':
-                                //specifies the read file in order to populate the universe
-				ifile = optarg;				
-                                break;
-                        case 'o':
-                		//specifies the output file to print the final state of the universe
-                		out_print = true;
-				ofile = optarg;
-		   		break;
-                        default:
-                                return 1;
+    while ((opt = getopt(argc, argv, "tsn:i:o:")) != -1) {
+        switch (opt) {
+        case 't':
+            //toggle toriodal on
+            isToroidal = true;
+            break;
+        case 's':
+            //Toggle Silence ncurses off
+            isNcurses = false;
+            break;
+        case 'n':
+            //specifies the number of generations
+            n = atoi(optarg);
+            break;
+        case 'i':
+            //specifies the read file in order to populate the universe
+            ifile = optarg;
+            break;
+        case 'o':
+            //specifies the output file to print the final state of the universe
+            out_print = true;
+            ofile = optarg;
+            break;
+        default: return 1;
+        }
+    }
+
+    FILE *file = fopen(ifile, "r");
+
+    uint64_t row, col;
+    fscanf(file, "%" SCNu64 "%" SCNu64 "\n", &row, &col);
+    //Main universe
+    Universe *u = uv_create(row, col, isToroidal);
+    //Next screen universe
+    Universe *b = uv_create(row, col, isToroidal);
+
+    uv_populate(u, file);
+    uv_populate(b, file);
+
+    if (isNcurses == true) {
+        initscr();
+        curs_set(FALSE);
+    }
+
+    for (uint32_t i = 0; i < n; i += 1) {
+        if (isNcurses == true) {
+            clear();
+        }
+        //create screen + play game
+        for (uint32_t r = 0; r < row; r += 1) {
+
+            for (uint32_t c = 0; c < col; c += 1) {
+                int alive = uv_census(u, r, c);
+
+                bool state = uv_get_cell(u, r, c);
+                //Below I commened out the prints I used to test the program,
+                //as well as the ones that print out the cell state
+                //fprintf(stdout, "POS = %d %d", r, c);
+                //fprintf(stdout, " state: %d",state);
+                //fprintf(stdout, " alive n = %d \n", alive);
+
+                if (state == true) {
+                    //Cell is alive
+                    if ((alive == 2) || (alive == 3)) {
+                        //Cell survives
+                        uv_live_cell(b, r, c);
+                        //fprintf(stdout, "Survives\n");
+                    } else {
+                        //Cell Dies
+                        uv_dead_cell(b, r, c);
+                        //fprintf(stdout, "Dies\n");
+                    }
+                } else if (state == false) {
+                    //Cell is dead
+                    if (alive == 3) {
+                        //Bring to life
+                        uv_live_cell(b, r, c);
+                        //fprintf(stdout,"Becomes Alive\n");
+                    } else {
+                        //stays dead
+                        fprintf(stdout, "Stays Dead\n");
+                        //uv_dead_cell(b ,r ,c);
+                    }
                 }
+            }
         }
 
-	FILE *file = fopen(ifile, "r");
-
-	uint64_t row, col;
-	fscanf(file, "%" SCNu64 "%" SCNu64 "\n", &row, &col);
-        //Main universe
-	Universe *u = uv_create(row, col, isToroidal);
-	//Next screen universe
-	Universe *b = uv_create(row, col ,isToroidal);
-
-        uv_populate(u, file);
-	uv_populate(b, file);
-		
- 	if (isNcurses == true) {
-                initscr();
-                curs_set(FALSE);
+        uint32_t rowsc = uv_rows(u);
+        uint32_t colsc = uv_cols(u);
+        //Generate the output of the live cells on the screen
+        for (uint32_t p = 0; p < rowsc; p += 1) {
+            for (uint32_t j = 0; j < colsc; j += 1) {
+                bool state = uv_get_cell(u, p, j);
+                if (state == true) {
+                    mvprintw(p, j, "o");
+                }
+            }
         }
 
-	for(uint32_t i = 0; i < n; i+= 1) {
-		if(isNcurses == true) {
-			clear();
-		}
-		//create screen + play game
-		for(uint32_t r = 0; r < row; r += 1) {
-			
-			for(uint32_t c = 0; c < col; c+=1 ) {
-				int alive = uv_census(u, r, c);
-				
-				bool state = uv_get_cell(u,r,c);
-				//Below I commened out the prints I used to test the program, 
-				//as well as the ones that print out the cell state
-				//fprintf(stdout, "POS = %d %d", r, c);
-				//fprintf(stdout, " state: %d",state);
-				//fprintf(stdout, " alive n = %d \n", alive);
+        //Swap Universes
+        Universe *temp = u;
+        u = b;
+        b = temp;
 
-				if (state == true) {
-				//Cell is alive
-					if ((alive == 2) || (alive == 3)) {
-						//Cell survives
-						uv_live_cell(b, r ,c);
-						//fprintf(stdout, "Survives\n");
-					} else {
-						//Cell Dies
-						uv_dead_cell(b, r, c);
-						//fprintf(stdout, "Dies\n");
-					}
-				} else if (state == false) {
-				//Cell is dead
-					if (alive == 3) {
-						//Bring to life
-						uv_live_cell(b,r,c);
-						//fprintf(stdout,"Becomes Alive\n");
-					} else {
-						//stays dead
-						fprintf(stdout, "Stays Dead\n");
-						//uv_dead_cell(b ,r ,c);
-					}
-				}	
-			}
-		}	
-		
-		uint32_t rowsc = uv_rows(u);
-		uint32_t colsc = uv_cols(u);
-		//Generate the output of the live cells on the screen
-		for (uint32_t p = 0; p < rowsc; p+= 1) {
-			for (uint32_t j = 0; j < colsc; j += 1) {
-				bool state = uv_get_cell(u, p, j);
-				if (state == true) {
-					mvprintw(p, j, "o");
-				}
-			}
-		}
+        if (isNcurses == true) {
+            refresh();
+            usleep(DELAY);
+        }
+    }
 
-		//Swap Universes		
-      		Universe *temp = u;
-                u = b;
-                b = temp;
+    endwin();
+    if (out_print == true) {
+        //print the last universe state in the 'output' file
+        FILE *outfile = fopen(ofile, "w");
+        uv_print(u, outfile);
+        fclose(outfile);
+    } else {
+        //by default prints the last universe state in console
+        uv_print(u, stdout);
+    }
+    uv_delete(u);
+    uv_delete(b);
+    fclose(file);
 
-		if(isNcurses == true) {
-			refresh();
-			usleep(DELAY);
-		}
-		
-	}
-
-	endwin();
-	if (out_print == true) {
-		//print the last universe state in the 'output' file
-		FILE *outfile = fopen(ofile, "w");
-		uv_print(u,outfile);
-		fclose(outfile);
-	} else {
-		//by default prints the last universe state in console
-		uv_print(u, stdout);
-	}
-	uv_delete(u);
-	uv_delete(b);
-        fclose(file);
-
-	return 0;
+    return 0;
 }
--- asgn4/universe.c
+++ asgn4/universe.c
@@ -6,162 +6,158 @@
 
 //Not Typedef
 struct Universe {
-	uint32_t rows;
-	uint32_t cols;
-	bool **grid;
-	bool toroidal;
+    uint32_t rows;
+    uint32_t cols;
+    bool **grid;
+    bool toroidal;
 };
 
-
 Universe *uv_create(uint32_t rows, uint32_t cols, bool toroidal) {
-	//Create a universe
-	bool **matrix = (bool **) calloc(rows, sizeof(bool *));
-	for (uint32_t r = 0; r < rows; r += 1) {
-		matrix[r] = (bool *) calloc(cols, sizeof(bool));
-	}
+    //Create a universe
+    bool **matrix = (bool **) calloc(rows, sizeof(bool *));
+    for (uint32_t r = 0; r < rows; r += 1) {
+        matrix[r] = (bool *) calloc(cols, sizeof(bool));
+    }
 
-	Universe *u = (Universe *)malloc(sizeof(Universe));
-	u->rows = rows;
-	u->cols = cols;
-	u->grid = matrix;
-	u->toroidal = toroidal;
-	return u;
+    Universe *u = (Universe *) malloc(sizeof(Universe));
+    u->rows = rows;
+    u->cols = cols;
+    u->grid = matrix;
+    u->toroidal = toroidal;
+    return u;
 }
 
 void uv_delete(Universe *u) {
-	//free up memory
-	free(u->grid);
-	free(u);
+    //free up memory
+    free(u->grid);
+    free(u);
 }
 
 uint32_t uv_rows(Universe *u) {
-	// return rows
-	return u->rows; 
+    // return rows
+    return u->rows;
 }
 
 uint32_t uv_cols(Universe *u) {
-	//return columns
-	return u->cols;
+    //return columns
+    return u->cols;
 }
 
 void uv_live_cell(Universe *u, uint32_t r, uint32_t c) {
-	//Marks cell at row -‘r’ , and column - ‘c’ as live
-	if ((r < (u->rows)) && (c < (u->cols))) {
-		u->grid[r][c] = 1;
-	}
+    //Marks cell at row -‘r’ , and column - ‘c’ as live
+    if ((r < (u->rows)) && (c < (u->cols))) {
+        u->grid[r][c] = 1;
+    }
 }
 
-
 void uv_dead_cell(Universe *u, uint32_t r, uint32_t c) {
-	//This function marks the cell of row -‘r’ , and column - ‘c’ as dead
-	if ((r < uv_rows(u)) && (c < uv_cols(u))) {
-		u->grid[r][c] = 0;
-	} else {
-		printf("Invalid Cell");
-	}
+    //This function marks the cell of row -‘r’ , and column - ‘c’ as dead
+    if ((r < uv_rows(u)) && (c < uv_cols(u))) {
+        u->grid[r][c] = 0;
+    } else {
+        printf("Invalid Cell");
+    }
 }
 
 bool uv_get_cell(Universe *u, uint32_t r, uint32_t c) {
-	// Returns the state of a cell of row -‘r’ , and column - ‘c’ 
-	if ((r < (u->rows)) && (c < (u->cols))) {
-		return u->grid[r][c]; 
-	}
+    // Returns the state of a cell of row -‘r’ , and column - ‘c’
+    if ((r < (u->rows)) && (c < (u->cols))) {
+        return u->grid[r][c];
+    }
 
-	return -1;
+    return -1;
 }
 
 bool uv_populate(Universe *u, FILE *infile) {
-	//This is the caller function and will populate the Universe with row-column pairs 
-	uint32_t rowsc, colsc;	
-	//fscanf(infile, "%d %d\n", &rowsc, &colsc);
-        rowsc = uv_rows(u);
-	colsc = uv_cols(u);
+    //This is the caller function and will populate the Universe with row-column pairs
+    uint32_t rowsc, colsc;
+    //fscanf(infile, "%d %d\n", &rowsc, &colsc);
+    rowsc = uv_rows(u);
+    colsc = uv_cols(u);
 
-	//assigns false to all values in the array
-        for (int i = 0; (uint32_t)i < rowsc; i+= 1) {
-                for (int j = 0; (uint32_t)j < colsc; j += 1) {
-                       uv_dead_cell(u, i, j);
-                }
+    //assigns false to all values in the array
+    for (int i = 0; (uint32_t) i < rowsc; i += 1) {
+        for (int j = 0; (uint32_t) j < colsc; j += 1) {
+            uv_dead_cell(u, i, j);
         }
-	//assigns true to the values specified in the file
+    }
+    //assigns true to the values specified in the file
 
-	int r, c;
-	while (fscanf(infile, "%d %d\n", &r, &c) != EOF) {
-		//fscanf(infile, "%d %d\n", &r, &c);
-		uv_live_cell(u, r, c);
-	}
+    int r, c;
+    while (fscanf(infile, "%d %d\n", &r, &c) != EOF) {
+        //fscanf(infile, "%d %d\n", &r, &c);
+        uv_live_cell(u, r, c);
+    }
 
-	return true;
+    return true;
 }
 
 uint32_t uv_census(Universe *u, uint32_t r, uint32_t c) {
-	//This function returns the number of live neighbors adjacent to the cell as row ‘r’ and column ‘c’.
-	
-	uint32_t alive = 0;
-	int rowsc = (int)uv_rows(u);
-	int colsc = (int)uv_cols(u);
-	bool **grd = u->grid;
-	
-	int row = (int) r;
-	int col = (int) c;
+    //This function returns the number of live neighbors adjacent to the cell as row ‘r’ and column ‘c’.
 
-	if (u->toroidal == false) {
-	 //Normal
-		for(int i=row-1; i<=row + 1; i+= 1) {
-       			 for(int j=col-1;j<=col + 1;j+= 1) {
-				if ((i==row && j==col) || (i>=rowsc) ||(j>=colsc) || ((j < 0) || (i < 0))) {
-           		     		continue;
-            			}	
+    uint32_t alive = 0;
+    int rowsc = (int) uv_rows(u);
+    int colsc = (int) uv_cols(u);
+    bool **grd = u->grid;
 
-            			if(grd[i][j]==1) {
-                			alive += 1;
-            			}
-				
-        		}
-    		}
-	} else {
+    int row = (int) r;
+    int col = (int) c;
 
-	//Toroidal
-	
-		for(int i=row-1; i<=row + 1; i+=1 ) {
-			for(int j=col-1; j<=col + 1; j += 1) {
-				//if top right
-				//if top left
-				//if bottom right
-				//if bottom right
-				//if top
-				//if bottom
-				//if left
-				//if right
-				
-				if (i==row && j==col) {
-					continue;
-				}
-				
-				if(grd[(i + rowsc) % rowsc][(j + colsc) % colsc] == 1) {
-					alive += 1;
-				}
-			}
-		}	
-	
-	}
-	return alive;
+    if (u->toroidal == false) {
+        //Normal
+        for (int i = row - 1; i <= row + 1; i += 1) {
+            for (int j = col - 1; j <= col + 1; j += 1) {
+                if ((i == row && j == col) || (i >= rowsc) || (j >= colsc)
+                    || ((j < 0) || (i < 0))) {
+                    continue;
+                }
+
+                if (grd[i][j] == 1) {
+                    alive += 1;
+                }
+            }
+        }
+    } else {
+
+        //Toroidal
+
+        for (int i = row - 1; i <= row + 1; i += 1) {
+            for (int j = col - 1; j <= col + 1; j += 1) {
+                //if top right
+                //if top left
+                //if bottom right
+                //if bottom right
+                //if top
+                //if bottom
+                //if left
+                //if right
+
+                if (i == row && j == col) {
+                    continue;
+                }
+
+                if (grd[(i + rowsc) % rowsc][(j + colsc) % colsc] == 1) {
+                    alive += 1;
+                }
+            }
+        }
+    }
+    return alive;
 }
 
 void uv_print(Universe *u, FILE *outfile) {
-	//Prints the universe in an ‘outfile’
-	uint32_t rowsc = uv_rows(u);
-	uint32_t colsc = uv_cols(u);
-	bool **grd = u->grid;
-	for (uint32_t i = 0; i < rowsc; i+= 1) {
-		for (uint32_t j = 0; j < colsc; j += 1) {
-			if (grd[i][j] == true) {
-				fprintf(outfile, "%s", "o");
-			} else if (grd[i][j] == false) {
-				fprintf(outfile, "%s", ".");
-			}
-		}
-		fprintf(outfile, "\n");
-	}
+    //Prints the universe in an ‘outfile’
+    uint32_t rowsc = uv_rows(u);
+    uint32_t colsc = uv_cols(u);
+    bool **grd = u->grid;
+    for (uint32_t i = 0; i < rowsc; i += 1) {
+        for (uint32_t j = 0; j < colsc; j += 1) {
+            if (grd[i][j] == true) {
+                fprintf(outfile, "%s", "o");
+            } else if (grd[i][j] == false) {
+                fprintf(outfile, "%s", ".");
+            }
+        }
+        fprintf(outfile, "\n");
+    }
 }
-
