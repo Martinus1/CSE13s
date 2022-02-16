ed70ad891d17a8b46e1e807be279396529f3405d
Martin Michalko <mmichalk>
2022-02-16 17:29:54 +0000

    008

--- asgn5/encrypt.c
+++ asgn5/encrypt.c
@@ -6,15 +6,16 @@
 #include <getopt.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <gmp.h>
 #include "rsa.h"
 #include "numtheory.h"
 #include "randstate.h"
 
 
 
-int main(int argc, char **argv) {
+int main() {
 
-	uint64_t iters = 100
+	uint64_t iters = 100;
 	//while ((opt = getopt(argc, argv, "tsn:i:o:")) != -1) {
 	//	switch(opt) {
 		
@@ -22,16 +23,16 @@ int main(int argc, char **argv) {
 	//}
 	
 	mpz_t a, b, c, d, e;
-	mpz_init_set(a, 1);
-	mpz_init_set(b, 2);
-	mpz_init_set(c, 4);
-	mpz_init_set(d, 7);
-	mpz_init_set(e, 269);
+	mpz_init_set_ui(a, 1);
+	mpz_init_set_ui(b, 2);
+	mpz_init_set_ui(c, 4);
+	mpz_init_set_ui(d, 7);
+	mpz_init_set_ui(e, 269);
 	is_prime(a, iters);
 	is_prime(b, iters);
-	isprint(c, iters);
-	isprint(d, iters);
-	isprint(e, iters);
+	is_prime(c, iters);
+	is_prime(d, iters);
+	is_prime(e, iters);
 
-	return;
+	return 0;
 }
